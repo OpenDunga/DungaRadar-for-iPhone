9f0a0f2597a6e1966e0e5110e5c9ada7f11c08f3
giginet <giginet.net@gmail.com>
2012-01-15 16:10:45 +0900

    Implement HttpAsyncConnection

---
+++ DungaRadar/models/HttpAsyncConnection.h
@@ -0,0 +1,13 @@
+//
+//  HttpAsyncConnection.h
+//  DungaRadar
+//
+//  Created by  on 1/15/12.
+//  Copyright (c) 2012 Kawaz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HttpAsyncConnection : NSObject
+
+@end
---
+++ DungaRadar/models/HttpAsyncConnection.m
@@ -0,0 +1,13 @@
+//
+//  HttpAsyncConnection.m
+//  DungaRadar
+//
+//  Created by  on 1/15/12.
+//  Copyright (c) 2012 Kawaz. All rights reserved.
+//
+
+#import "HttpAsyncConnection.h"
+
+@implementation HttpAsyncConnection
+
+@end
